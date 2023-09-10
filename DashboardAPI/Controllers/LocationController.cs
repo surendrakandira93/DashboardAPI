88872f07a47d21fe210a57635309fb81ab12d831
Surendra Kandira <surendrakandira@gmail.com>
2023-09-10 14:37:25 +0530

    added post

--- DashboardAPI/Controllers/LocationController.cs
+++ DashboardAPI/Controllers/LocationController.cs
@@ -24,5 +24,21 @@ namespace DashboardAPI.Controllers
             };
             return new ResponseDto<List<SelectedListItemDto>>() { Result = locationList, IsSuccess = true };
         }
+
+        [HttpPost]
+        public ResponseDto<List<SelectedListItemDto>> Add(SelectedListItemDto model)
+        {
+            var locationList = new List<SelectedListItemDto>()
+            {
+                new SelectedListItemDto(){ Id=1,Name="Brighton"},
+                new SelectedListItemDto(){ Id=2,Name="Camberley"},
+                new SelectedListItemDto(){ Id=3,Name="Southamption"},
+                new SelectedListItemDto(){ Id=4,Name="Birmingham"},
+                new SelectedListItemDto(){ Id=5,Name="Leicester"},
+                new SelectedListItemDto(){ Id=6,Name="Londan"}
+            };
+            locationList.Add(model);
+            return new ResponseDto<List<SelectedListItemDto>>() { Result = locationList, IsSuccess = true };
+        }
     }
 }
